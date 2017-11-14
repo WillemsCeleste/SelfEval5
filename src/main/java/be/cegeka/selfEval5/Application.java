f694aaca6c4374f9960c0a18014f4a53c84020cd
Celeste Willems <willems.celeste@gmail.com>
2017-11-14 10:29:49 +0100

    application setup

--- src/main/java/be/cegeka/selfEval5/Application.java
+++ src/main/java/be/cegeka/selfEval5/Application.java
@@ -1,4 +1,4 @@
-package be.cegeka.selfEval5.application;
+package be.cegeka.selfEval5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
